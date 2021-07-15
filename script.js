192ea6256f8d066b80665da819e9a54ce358788c
ArtsemYakautsou89 <tema89_21@mail.ru>
2021-07-16 02:02:12 +0300

    Modal window, end of the working day

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 // header sticky
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-        $("header").addClass("sticky");
-    } else {
-        $("header").removeClass("sticky");
-    }
-});
-// button Up
-$('body').append('<div class="upbtn"></div>');
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 400) {
-        $('.upbtn').css({
-            transform: 'scale(.9)'
-        });
-    } else {
-        $('.upbtn').css({
-            transform: 'scale(0)'
-        });
-    }
-});
-$('.upbtn').on('click', function() {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 500);
-    return false;
-});
-// add dynamically html
-function windowSize() {
-    if ($(window).width() <= '601') {
-        $('.menu__box').append('<li><div class="menu-phone visual"><a href="tel:+375115555555" target="_blank" title="Звоните!">+375 11 555-55-55</a><a href="tel:+375115555555" target="_blank" title="Звоните!">+375 11 555-55-55</a></div></li>');
-    } else {}
-}
-$(window).load(windowSize);
-
-function windowSize2() {
-    if ($(window).width() <= '426') {
-        $('.visual').append('<li><button class="menu-advice-btn seeable">Консультация</button></li>');
-    } else {}
-}
-$(window).load(windowSize2);
-
-// accordeon
 $(document).ready(function() {
-    $(".accordeon").find("dd:last").show();
-});
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $("header").addClass("sticky");
+        } else {
+            $("header").removeClass("sticky");
+        }
+    });
 
-$(".accordeon dd").hide().prev().click(function() {
-    $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
-    $(this).next().not(":visible").slideDown().prev().addClass("active");
-});
+    // button Up
+    $('body').append('<div class="upbtn"></div>');
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            $('.upbtn').css({
+                transform: 'scale(.9)'
+            });
+        } else {
+            $('.upbtn').css({
+                transform: 'scale(0)'
+            });
+        }
+
+        $('.upbtn').on('click', function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    });
+    // add dynamically html
+    function windowSize() {
+        if ($(window).width() <= '601') {
+            $('.menu__box').append('<li><div class="menu-phone visual"><a href="tel:+375115555555" target="_blank" title="Звоните!">+375 11 555-55-55</a><a href="tel:+375115555555" target="_blank" title="Звоните!">+375 11 555-55-55</a></div></li>');
+        } else {}
+    }
+    $(window).load(windowSize);
+
+    // accordeon
+    $(document).ready(function() {
+        $(".accordeon").find("dd:last").show();
+    });
+
+    $(".accordeon dd").hide().prev().click(function() {
+        $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
+        $(this).next().not(":visible").slideDown().prev().addClass("active");
+    });
+
+    //modal form
+    $('.menu-advice-btn').click(function() {
+        $('.contact-form').show();
+        $('.modal').show();
+    });
+    $('.close').click(function() {
+        $('.contact-form').hide();
+        $('.modal').hide();
+
+    });
+})
